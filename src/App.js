76fea86a757857ff362a3944a83e0a64f32b23c7
RickyAnchores <ricky23anchores@gmail.com>
2022-06-03 17:29:37 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        DEPLOYMENT FROM GITHUB YEAHHH!   
+        DEPLOYMENT FROM GITHUB YEAHHH!  
+        NEW UPDATE FROM GITHUB 
       </header>
     </div>
   );
